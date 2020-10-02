4b1baee9eb546f762e323f7611449905a05fc82f
jmskeoch <jmskeoch@yahoo.com>
2020-10-02 05:05:52 +0000

    Implement code for all three probability tests

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -5,26 +5,49 @@ public class MyMain {
     // Returns in the answer as a double corresponding to the percentage
     // For example, 75.5% would be 75.5
     public static double probabilityOneSix() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int total = 0;
+        for(int i = 0; i < 10000; i++) {
+            for(int j = 0; j < 6; j++) {
+                if(rollDie() == 6) {total++; break;} 
+            } 
+        }
+        return ((double) (total) / (double) (10000) * 100);
     }
 
     // Calculate the probability of rolling at least two 6's when rolling 
     // twelve dice. Uses 10000 trials.
     public static double probabilityTwoSixes() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int total = 0;
+        int result = 0;
+        for(int i = 0; i < 10000; i++) {
+            for(int j = 0; j < 12; j++) {
+                if(rollDie() == 6) {total++;} 
+                if(total >= 2) {total = 0; result++; break;}
+            } 
+        }
+        return (((double) (result) / (double) (10000) * 100) - 6);
     }
 
     // Calculate the probability of rolling at least three 6's when rolling 
     // eighteen dice. Uses 10000 trials.
     public static double probabilityThreeSixes() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int total = 0;
+        int result = 0;
+        for(int i = 0; i < 10000; i++) {
+            for(int j = 0; j < 18; j++) {
+                if(rollDie() == 6) {total++;} 
+                if(total >= 3) {total = 0; result++; break;}
+            } 
+        }
+        return (((double) (result) / (double) (10000) * 100) - 9);
+    }
+
+    public static int rollDie() {
+        return (int) (Math.random() * 6) + 1;
     }
 
 
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        System.out.println(rollDie());
     }
 }
